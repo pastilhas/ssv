b501db1975273a83af1787b6fa0097336de4fbb8
Pastilhas <joao.campos1221@gmail.com>
2024-01-26 00:45:42 +0000

    unnecessary append

--- src/checksum.v
+++ src/checksum.v
@@ -1,13 +1,11 @@
 module main
 
 import encoding.binary
-import arrays
 
 pub fn checksum(bytes []u8) u16 {
 	mut sum := u32(0)
-	bytes_ := arrays.append(bytes, [u8(0x00)])
 	for i := 0; i < bytes.len; i += 2 {
-		val := binary.big_endian_u16_at(bytes_, i)
+		val := binary.big_endian_u16_at(bytes, i)
 		sum += val
 	}
 	for sum >> 16 != 0 {
